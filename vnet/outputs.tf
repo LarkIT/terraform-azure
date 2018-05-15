c45485bb8af8410f5f61adc4c4d880e1e916939c
Nick Fosdick <nick@lark-it.com>
2018-05-15 16:41:49 -0600

    Fixing outputs

--- vnet/outputs.tf
+++ vnet/outputs.tf
@@ -11,13 +11,13 @@ output "rg_name" {
 }
 
 output "dmz_subnet_id" {
-  value = "${azurerm_subnet.test_dmz_subnet.id}"
+  value = "${azurerm_subnet.dmz_subnet.id}"
 }
 
 output "app_subnet_id" {
-  value = "${azurerm_subnet.test_app_subnet.id}"
+  value = "${azurerm_subnet.app_subnet.id}"
 }
 
 output "db_subnet_id" {
-  value = "${azurerm_subnet.test_db_subnet.id}"
+  value = "${azurerm_subnet.db_subnet.id}"
 }
