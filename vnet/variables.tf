8859138d4778613350131419d4fc15a1a0d5b06f
Nick Fosdick <nick@lark-it.com>
2018-05-15 16:04:46 -0600

    Removing variables

--- vnet/variables.tf
+++ vnet/variables.tf
@@ -1,22 +1,9 @@
 variable "application_name" {}
 variable "environment" {}
 variable "location" {}
-variable "resource_group_name" {
-  default = ""
-}
 
 variable "address_space" {
   default = "10.10.0.0/16"
-}
-
-variable "test_network" {
-  description = "Subnet layout for network zones"
-
-  default = {
-    test_dmz = "10.10.0.0/24"
-    test_app = "10.10.10.0/24"
-    test_db  = "10.10.20.0/24"
-  }
 }
 
 variable "network" {
