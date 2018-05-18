7279b9d96dbf38d0d86b85d584f968cd545df3f0
Nick Fosdick <nick@lark-it.com>
2018-05-18 15:25:16 -0600

    adding subnet to agw

--- agw/main.tf
+++ agw/main.tf
@@ -7,6 +7,13 @@ resource "azurerm_resource_group" "rg" {
   location = "${var.location}"
 }
 
+resource "azurerm_subnet" "subnet" {
+  name                 = "${var.environment}_${var.application_name}_agw_subnet"
+  resource_group_name  = "${azurerm_resource_group.rg.name}"
+  virtual_network_name = "${azurerm_virtual_network.vnet.name}"
+  address_prefix       = "10.254.0.0/24"
+}
+
 resource "azurerm_public_ip" "agw_pip" {
   name                         = "${var.environment}_${var.application_name}_agw_pip"
   location                     = "${var.location}"
