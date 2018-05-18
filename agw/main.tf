64c6ea6cc3d2af0ba21b82f683a89092ae4a0cb2
Nick Fosdick <nick@lark-it.com>
2018-05-18 17:13:47 -0600

    fixing name

--- agw/main.tf
+++ agw/main.tf
@@ -10,7 +10,7 @@ resource "azurerm_resource_group" "rg" {
 resource "azurerm_subnet" "subnet" {
   name                 = "${var.environment}_${var.application_name}_agw_subnet"
   resource_group_name  = "${azurerm_resource_group.rg.name}"
-  virtual_network_name = "${azurerm_virtual_network.vnet.name}"
+  virtual_network_name = "${var.vnet_name}"
   address_prefix       = "10.254.0.0/24"
 }
 
