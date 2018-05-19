f74cff3b0a6ad2baed85b2804d97de2aee36313c
Nick Fosdick <nick@lark-it.com>
2018-05-18 19:18:59 -0600

    harding coding name

--- agw/main.tf
+++ agw/main.tf
@@ -70,11 +70,11 @@ resource "azurerm_application_gateway" "network" {
     protocol                       = "Http"
   }
 
-  request_routing_rule {
-    name                       = "${var.vnet_name}-rqrt"
-    rule_type                  = "Basic"
-    http_listener_name         = "${var.vnet_name}-httplstn"
-    backend_address_pool_name  = "${var.vnet_name}-beap"
-    backend_http_settings_name = "${var.vnet_name}-be-htst"
-  }
+#  request_routing_rule {
+#    name                       = "${var.vnet_name}-rqrt"
+#    rule_type                  = "Basic"
+#    http_listener_name         = "${var.vnet_name}-httplstn"
+#    backend_address_pool_name  = "${var.vnet_name}-beap"
+#    backend_http_settings_name = "${var.vnet_name}-be-htst"
+#  }
 }
