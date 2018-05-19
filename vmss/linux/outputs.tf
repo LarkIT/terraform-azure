53d5ff98b49910a1d51a642197cdfe09ffe1a883
Nick Fosdick <nick@lark-it.com>
2018-05-18 21:09:10 -0600

    adding exported values

--- vmss/linux/outputs.tf
+++ vmss/linux/outputs.tf
@@ -1,3 +1,13 @@
+azurerm_lb" "loadbalancer
+
+output "lb_id {
+  value = "${azurerm_lb.loadbalancer.id}"
+}
+
+output "lb_ip_address" {
+  value = "${azurerm_lb.loadbalancer.private_ip_address}"
+}
+
 output "bpepool_id" {
   value = "${azurerm_lb_backend_address_pool.bpepool.id}"
 }
