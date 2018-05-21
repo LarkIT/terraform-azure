7ed6bb93bbeaf0c88cbfd22836e4b66b0190f5ce
Nick Fosdick <nick@lark-it.com>
2018-05-20 20:20:05 -0600

    Removing comments

--- vmss/linux/main.tf
+++ vmss/linux/main.tf
@@ -110,8 +110,6 @@ resource "azurerm_virtual_machine_scale_set" "vmss" {
       name                                   = "TestIPConfiguration"
       subnet_id                              = "${var.subnet_id}"
       load_balancer_backend_address_pool_ids = ["${azurerm_lb_backend_address_pool.bpepool.id}"]
-
-      #      load_balancer_inbound_nat_rules_ids    = ["${element(azurerm_lb_nat_pool.lbnatpool.*.id, count.index)}"]
     }
   }
 }
