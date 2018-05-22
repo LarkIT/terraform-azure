8bf0006b51da45dfebedd5a5499fd33b92cbfcc5
Nick Fosdick <nick@lark-it.com>
2018-05-21 20:25:26 -0600

    adding domain to test

--- vnet/variables.tf
+++ vnet/variables.tf
@@ -11,10 +11,12 @@ variable "network" {
 
   default = {
     test = {
-      dmz = "10.10.0.0/24"
-      app = "10.10.10.0/24"
-      db  = "10.10.20.0/24"
-    } 
+      dmz    = "10.10.0.0/24"
+      app    = "10.10.10.0/24"
+      db     = "10.10.20.0/24"
+      domain = "10.10.30.0/24"
+    }
+
     prod = {
       dmz    = "10.10.100.0/24"
       app    = "10.10.110.0/24"
@@ -27,6 +29,7 @@ variable "network" {
 variable "agw_subnet" {
   default = "10.10.30.0/24"
 }
+
 variable "dns_servers" {
   default = ["10.10.0.7", "10.10.0.8"]
 }
