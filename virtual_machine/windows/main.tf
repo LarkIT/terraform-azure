028732e63ac447af642c7b46f47d2c1d5f3da096
Nick Fosdick <nick@lark-it.com>
2018-05-29 22:42:43 -0600

    [them-60] removing comments

--- virtual_machine/windows/main.tf
+++ virtual_machine/windows/main.tf
@@ -1,7 +1,6 @@
 resource "azurerm_public_ip" "public_ip" {
   count                        = "${var.number_servers}"
   name                         = "${var.application_name}_${var.hostname}_public_ip_${count.index + var.start_index}"
-#  name                         = "${var.application_name}_${var.hostname}_public_ip_${count.index}"
   location                     = "${var.location}"
   resource_group_name          = "${var.resource_group}"
   public_ip_address_allocation = "${var.public_ip_allocation}"
@@ -14,18 +13,15 @@ resource "azurerm_public_ip" "public_ip" {
 resource "azurerm_network_interface" "nic" {
   count                     = "${var.number_servers}"
   name                      = "${var.application_name}_${var.hostname}_nic_${count.index + var.start_index}"
-#  name                      = "${var.application_name}_${var.hostname}_nic_${count.index}"
   location                  = "${var.location}"
   resource_group_name       = "${var.resource_group}"
   network_security_group_id = "${var.security_group}"
 
   ip_configuration {
     name                          = "${var.application_name}_${var.hostname}_nic_config_${count.index + var.start_index}"
-    #name                          = "${var.application_name}_${var.hostname}_nic_config_${count.index}"
     subnet_id                     = "${var.subnet_id}"
     private_ip_address_allocation = "dynamic"
-    #public_ip_address_id          = "${element(azurerm_public_ip.public_ip.*.id, count.index + var.start_index)}"
-    public_ip_address_id          = "${element(azurerm_public_ip.public_ip.*.id, count.index)}"
+    public_ip_address_id          = "${element(azurerm_public_ip.public_ip.*.id, count.index + var.start_index)}"
   }
 
   tags {
@@ -35,7 +31,6 @@ resource "azurerm_network_interface" "nic" {
 
 resource "azurerm_virtual_machine" "virtual_machine" {
   count                            = "${var.number_servers}"
-  #name                             = "${var.application_name}_${var.hostname}_vm_${count.index + + var.start_index}"
   name                             = "${var.application_name}_${var.hostname}_vm_${count.index + var.start_index}"
   location                         = "${var.location}"
   resource_group_name              = "${var.resource_group}"
