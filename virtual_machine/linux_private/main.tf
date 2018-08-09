26a4152e558b4b9ce99b49cde7491a50c4c2fe06
Costa Arigirs <costa.argiris@lark-it.com>
2018-08-08 18:25:34 -0600

    Added module for private linux virtual machine
    THEM-41

---
+++ virtual_machine/linux_private/main.tf
@@ -0,0 +1,59 @@
+locals { 
+  storage_image_reference = "${var.storage_image_reference[ "${var.server_type}" ]}"
+}
+
+resource "azurerm_network_interface" "nic" {
+  count                     = "${var.number_servers}"
+  name                      = "${var.application_name}_${var.hostname}_nic_${count.index}"
+  location                  = "${var.location}"
+  resource_group_name       = "${var.resource_group}"
+  network_security_group_id = "${var.security_group}"
+
+  ip_configuration {
+    name                          = "${var.application_name}_${var.hostname}_nic_config_${count.index}"
+    subnet_id                     = "${var.subnet_id}"
+    private_ip_address_allocation = "${var.nic_ip_allocation}"
+    private_ip_address            = "${var.private_ip_address}"
+  }
+
+  tags {
+    builtby = "Terraform"
+  }
+}
+
+resource "azurerm_virtual_machine" "virtual_machine" {
+  count                            = "${var.number_servers}"
+  name                             = "${var.application_name}_${var.hostname}_vm_${count.index}"
+  location                         = "${var.location}"
+  resource_group_name              = "${var.resource_group}"
+  network_interface_ids            = ["${element(azurerm_network_interface.nic.*.id, count.index)}"]
+  vm_size                          = "${var.vm_size}"
+  delete_os_disk_on_termination    = true
+  delete_data_disks_on_termination = true
+
+  storage_os_disk {
+    name              = "${var.application_name}_${var.hostname}_osdisk_${count.index}"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "${var.managed_disk_type}"
+    disk_size_gb      = "${var.os_disk_size}"
+  }
+
+  os_profile {
+    computer_name  = "${var.hostname}-${count.index}"
+    admin_username = "${var.admin_username}"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = true
+
+    ssh_keys {
+      path     = "/home/${var.admin_username}/.ssh/authorized_keys"
+      key_data = "${file("${var.ssh_public_key}")}"
+    }
+  }
+
+  tags {
+    builtby = "Terraform"
+  }
+}
---
+++ virtual_machine/linux_private/variables.tf
@@ -0,0 +1,53 @@
+variable "resource_group" {}
+variable "location" {}
+variable "environment" {}
+variable "application_name" {}
+variable "subnet_id" {}
+variable "hostname" {}
+variable "ssh_public_key" {}
+variable "security_group" {}
+
+variable "private_ip_address" {
+  default = ""
+}
+
+variable "nic_ip_allocation" {
+  default = "dynamic"
+}
+
+variable "admin_username" {
+  default = "azure"
+}
+
+variable "number_servers" {
+  default = "1"
+}
+
+variable "os_disk_size" {
+  default = "128"
+}
+
+variable "managed_disk_type" {
+  default = "Premium_LRS"
+}
+
+variable "vm_size" {
+  default = "Standard_DS1_v2"
+}
+
+variable "storage_image_reference" {
+  default = {
+    centos_server = {    
+      publisher = "OpenLogic"
+      offer     = "CentOS"
+      sku       = "7.5"
+      version   = "latest"
+    }
+    ubuntu_server = {    
+      publisher = "Canonical"
+      offer     = "UbuntuServer"
+      sku       = "16.04.0-LTS"
+      version   = "latest"
+    }
+  }
+}
